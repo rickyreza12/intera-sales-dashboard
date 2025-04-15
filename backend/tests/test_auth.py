9c57715acd3bc051b11ed9825d8584fbd633604b
rickyreza12 <muuhamadrickyreza@gmail.com>
2025-04-15 13:45:05 +0700

    test(auth): add unit tests for login, token validation, and protected enpoint access

---
+++ backend/tests/test_auth.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+import re
+from fastapi.testclient import TestClient
+from jose import jwt
+from main import app
+
+
+SECRET_KEY = "8897ABC"
+ALGORITHM = "HS256"
+
+client = TestClient(app)
+
+def test_login_success():
+    response = client.post("/api/token", data={"username": "admin", "password": "password123"})
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+    
+def test_login_failure():
+    response = client.post("/api/token", data = {"username": "admin", "password": "wrong"})
+    assert response.status_code == 401
+    assert response.json()["detail"]["message"] == "Invalid Credentials"
+
+def test_login_missing_fields():
+    response = client.post("/api/token", data={})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "Field required"
+
+def test_token_structure():
+    response = client.post("/api/token", data={"username":"admin", "password":"password123"})
+    assert response.status_code == 200
+    token = response.json()["access_token"]
+    
+    assert re.match(r"^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$", token)
+
+def test_protected_endpoint_no_token():
+    response = client.post("/api/sales-reps")
+    assert response.status_code == 401
+    assert response.json()["detail"]["message"] == "Missing or invalid token"
+
+def test_protected_endpoint_valid_token():
+    login = client.post("/api/token", data={"username":"admin", "password": "password123"})
+    token = login.json()["access_token"]
+    
+    headers = {"Authorization": f"Bearer {token}"}
+    response = client.post("/api/sales-reps", headers=headers)
+    
+    assert response.status_code == 200
+    assert response.json()["data"] is not None
+    assert isinstance(response.json()["data"], list)
+    
+def test_token_expired_logic():
+    expire = datetime.utcnow() - timedelta(minutes=1)
+    payload = {"sub": "admin", "exp": expire}
+    expired_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    
+    headers = {"Authorization": f"Bearer {expired_token}"}
+    response = client.post("/api/sales-reps", headers=headers)
+    
+    assert response.status_code == 401
+    assert response.json()["detail"]["message"] == "Invalid token"
